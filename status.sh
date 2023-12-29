914f9bba2cc8fa0ab367eea8e7b74cc5068314e1
francescobianco <info.francescobianco@gmail.com>
2023-12-29 17:44:17 +0100

    First commit

--- status.sh
+++ status.sh
@@ -1,9 +1,29 @@
 #!/bin/sh
+set -e
 
-while true;
-  do echo \
-   -e "HTTP/1.1 200 OK\n\n$(iostat)" \
-  | nc 0.0.0.0 8080;
-done
+cache() {
+    local ttl
 
+    ttl=$1
 
+    shift
+    "$@"
+}
+
+response() {
+    echo "HTTP/1.1 200 OK"
+    echo "Access-Control-Allow-Origin: *"
+    echo
+
+    echo " 192.168.144.10 . . . [OK]"
+
+    #cache 5 ls -la
+}
+
+main() {
+    while true;
+        do cache 5 response | ncat -l -p 8080
+    done
+}
+
+main
